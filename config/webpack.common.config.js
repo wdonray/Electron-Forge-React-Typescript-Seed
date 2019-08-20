571b6c7e5bc557fef23f7d7d3c8220039c7ecdbd
Donray Williams <donray_williams@gemvision.com>
2019-08-20 11:02:25 -0500

    Lodash added

--- config/webpack.common.config.js
+++ config/webpack.common.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+let LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 const PATH_TO_ROOT = path.resolve(__dirname, '..');
 
 /**
@@ -17,8 +17,19 @@ module.exports = {
             '@renderer': path.resolve(PATH_TO_ROOT, 'src/renderer/'),
         }
     },
+    plugins: [
+        new LodashModuleReplacementPlugin
+    ],
     module: {
         rules: [
+            {
+                test: /\.js$/,
+                loader: `babel-loader`,
+                options: {
+                    plugins: ['lodash'],
+                },
+                exclude: /node_modules/
+            },
             {
                 test: /\.tsx?$/,
                 loader: 'ts-loader',
--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
+import * as _ from 'lodash';
 export interface HelloProps { compiler: string; framework: string; }
 
 // 'HelloProps' describes the shape of props.
 // State is never set so we use the '{}' type.
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+        return <h1>Hello from {_.lowerCase(this.props.compiler)} and {this.props.framework}!</h1>;
     }
 }
