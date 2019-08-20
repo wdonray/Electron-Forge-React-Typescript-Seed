b1171b96b3baaba685d3951d0fd4c9adb42881ab
Donray Williams <donray_williams@gemvision.com>
2019-08-20 11:09:30 -0500

    Material UI added

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 import * as _ from 'lodash';
 export interface HelloProps { compiler: string; framework: string; }
+import {Button} from "@material-ui/core";
 
 // 'HelloProps' describes the shape of props.
 // State is never set so we use the '{}' type.
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
-        return <h1>Hello from {_.lowerCase(this.props.compiler)} and {this.props.framework}!</h1>;
+        return <div><Button onClick={() => console.log(_.random(0, 100))}>WORK</Button> </div>;
     }
 }
